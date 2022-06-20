2727f00d595886cdc7264d26ea309266ce5b32a6
aidyn <aidynsemey7sk@gmail.com>
2022-06-20 17:42:12 +0600

    leads

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -7,15 +7,25 @@
         </div>
 
         <div class="navbar-menu">
+
             <div class="navbar-end">
+                <router-link to="/dashboard/leads" class="navbar-item">Leads</router-link>
+
+
                 <div class="navbar-item">
                     <div class="buttons">
-                        <router-link to="/sign-up" class="button is-success">
-                            <strong>Регистрация</strong>
-                        </router-link>
-                        <router-link to="/log-in" class="button is-ligth">
-                            <strong>Вход</strong>
-                        </router-link>
+                        <template v-if="!$store.state.isAuthenticated">
+                            <router-link to="/sign-up" class="button is-success">
+                                <strong>Регистрация</strong>
+                            </router-link>
+                            <router-link to="/log-in" class="button is-ligth">
+                                <strong>Вход</strong>
+                            </router-link>
+                        </template>
+
+                        <template v-else>
+                            <router-link to="/dashboard/my-account" class="button is-info">My account</router-link>
+                        </template>
                     </div>
                 </div>
             </div>
---
+++ src/views/dashboard/Lead.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">{{lead.title}}</h1>
+            </div>
+        </div>
+    </div>
+</template>
